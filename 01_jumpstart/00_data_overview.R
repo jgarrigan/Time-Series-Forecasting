f057b6abb80c000626185a4c65ee9fb16d1fff0d
jgarrigan <jgarrigan@gmail.com>
2023-05-02 13:16:41 +0100

    Initial commit

---
+++ 01_jumpstart/00_data_overview.R
@@ -0,0 +1,37 @@
+# BUSINESS SCIENCE UNIVERSITY
+# DS4B 203-R: ADVANCED TIME SERIES FORECASTING FOR BUSINESS
+# MODULE: INSPECT COURSE DATASETS
+
+
+# LIBRARIES ----
+library(tidyverse)
+
+
+# DATA -----
+
+# * Establish Relationships ----
+#   - Website traffic (Page Views, Sessions, Organic Traffic)
+#   - Top 20 Pages
+
+# GOOGLE ANALYTICS SUMMARY HOURLY
+read_rds("00_data/google_analytics_summary_hourly.rds")
+
+# GOOGLE ANALYTICS TOP 20 PAGES DAILY
+read_rds("00_data/google_analytics_by_page_daily.rds")
+
+# * Build Relationships ----
+#   - Collect emails
+#   - Host Events
+
+# MAILCHIMP DATA
+read_rds("00_data/mailchimp_users.rds")
+
+read_rds("00_data/learning_labs.rds")
+
+# * Generate Course Revenue ----
+#   - Revenue data (aggregated at weekly interval)
+#   - Product Events
+
+read_rds("00_data/transactions_weekly.rds")
+
+read_rds("00_data/product_events.rds")
